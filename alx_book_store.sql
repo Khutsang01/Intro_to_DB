db3d36756ce10641dcfadfdf745642d0bd9f936d
Khutsang01 <khutsang01@gmail.com>
2025-08-17 01:59:24 +0200

    Fixing foreign key on the Orders table

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -33,7 +33,7 @@ CREATE TABLE Customers (
 -- Create the Orders table
 CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
-    customer_id INT FOREIGN KEY,
+    customer_id INT,
     order_date DATE NOT NULL,
     FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
 );
