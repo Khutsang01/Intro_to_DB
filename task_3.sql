2d15ded1dbcbfed7a77fcc4583beddd5e770d482
Khutsang01 <khutsang01@gmail.com>
2025-08-17 23:11:54 +0200

    Adding the 1st modification of task_3 for showing the tables created in the db

--- task_3.sql
+++ task_3.sql
@@ -1,5 +1,5 @@
 -- List all the tables created
 
--- The database name is passed as an argument to the mysql command,
--- so a 'USE' statement is not required here.
+USE alx_book_store;
+
 SHOW TABLES;
