740882cb96b2b7a04316b025264765bb7daf6337
Khutsang01 <khutsang01@gmail.com>
2025-08-17 23:46:42 +0200

    Add first data into the datebase

---
+++ task_5.sql
@@ -0,0 +1,4 @@
+-- Populating Your Database with Your Very First Data
+
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
