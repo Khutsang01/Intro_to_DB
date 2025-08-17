45278544a203eb990f215f0e547c333a19c622f7
Khutsang01 <khutsang01@gmail.com>
2025-08-17 20:32:03 +0200

    Add 2nd modification of alx book store sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -35,7 +35,8 @@ CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE NOT NULL,
-    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
+    -- This is the corrected line with a space before the parenthesis
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 -- Create the Order_Details table
